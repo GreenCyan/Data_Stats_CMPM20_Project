3fcd502eae1be87cf4bb0dc141b8bedf770b4cb1
GreenCyan <GreenCyan@users.noreply.github.com>
2015-02-08 16:01:07 -0800

    Create Hack_Stats.js

---
+++ Hack_Stats.js
@@ -0,0 +1,12 @@
+// Current day. Every week starts with Monday
+var theDay = 0;
+// positive numbers closer to government, negative numbers closer to freedom fighters
+var govAlignment = 30;
+// The hacker's budget
+var money = 0;
+// The hacker's stats
+var attack = 5;
+var defense = 5;
+var speed = 5;
+var mask = 5;
+// 
